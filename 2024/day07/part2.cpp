8a27ddaeab62f449a32e14ba1ffc94c122210e21
Artyom Andreev <aandreev06.1998@gmail.com>
2024-12-07 22:32:20 +0300

    Complete 2024, day 7

---
+++ 2024/day07/part2.cpp
@@ -0,0 +1,63 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using op_iterator = std::vector<size_t>::const_iterator;
+
+size_t concat_nums(size_t first, size_t second) {
+    std::stringstream oss;
+    oss << first;
+    oss << second;
+
+    size_t new_num;
+    oss >> new_num;
+
+    return new_num;
+}
+
+bool build_equation(size_t value, size_t cur, op_iterator op_it, op_iterator op_it_end) {
+    if (op_it == op_it_end) {
+        return value == cur;
+    }
+    if (cur > value) {
+        return false;
+    }
+
+    size_t new_cur;
+    return !__builtin_add_overflow(cur, *op_it, &new_cur) &&
+               build_equation(value, cur + *op_it, op_it + 1, op_it_end) ||
+           !__builtin_mul_overflow(cur, *op_it, &new_cur) &&
+               build_equation(value, cur * *op_it, op_it + 1, op_it_end) ||
+           build_equation(value, concat_nums(cur, *op_it), op_it + 1, op_it_end);
+}
+
+int main() {
+    std::ifstream input("input.txt");
+
+    size_t res = 0;
+    std::string line;
+    while (std::getline(input, line)) {
+        std::istringstream iss(line);
+
+        size_t value;
+        iss >> value;
+
+        iss.get();
+
+        size_t num;
+        std::vector<size_t> nums;
+        while (iss >> num) {
+            nums.push_back(num);
+        }
+
+        if (build_equation(value, nums.front(), nums.cbegin() + 1, nums.cend())) {
+            res += value;
+        }
+    }
+
+    std::cout << res << std::endl;
+
+    return 0;
+}
